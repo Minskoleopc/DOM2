2f4c9d057afffd1d9a94ba9f50b724180bc1eaa8
unknown <Minskoleopc>
2023-09-01 08:25:58 -0700

    added

---
+++ DOM10/script.js
@@ -0,0 +1,22 @@
+// <h1 id = "one" class = "two" name = "three">Heading One</h1>
+
+let headOne = document.querySelector('h1')
+console.log(headOne)
+
+headOne.addEventListener('click',function(){
+    headOne.style.color = "red"
+   
+})
+
+
+
+// let info = {
+//     fullName:"chinmay deshpande",
+//     parent:{
+//         mother:"kanchan deshpande",
+//         father:"shirish deshpande"
+//     }
+// }
+
+// info.parent.father = "shirish s deshpande"
+// console.log(info)
---
+++ DOM11/script.js
@@ -0,0 +1,9 @@
+let headone = document.querySelector('h1')
+let button = document.querySelector('button')
+
+console.log(headone)
+console.log(button)
+
+button.addEventListener('click',function(){
+    headone.style.color = "blue"
+})
