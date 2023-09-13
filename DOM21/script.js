c00ca3cbb58845a1e81ef7b38bf0ac0a2a758c72
unknown <Minskoleopc>
2023-09-13 07:32:36 -0700

    added

---
+++ DOM21/script.js
@@ -0,0 +1,35 @@
+// <h1 id = "one"  class = "three" name = "nm">Chinmay</h1>
+{/* <h1 id = "one" class = "two" name = "nm1">headOne</h1>
+<ul>
+    <li class = "fruit" name = "nm">Apple</li>
+    <li class = "fruit" name = "nm">Banana</li>
+    <li class = "fruit" name = "nm">Grapes</li>
+    <li class = "fruit" name = "nm">Mango</li>
+</ul> */}
+
+
+// document.querySelector('#one')
+// document.querySelector(".two")
+// document.querySelector("h1")
+// document.querySelector("h1[name ='nm']")
+// document.querySelectorAll('li')
+// document.querySelectorAll('.fruit')
+// document.querySelectorAll('li[name = "nm"]')
+
+// document.getElementById("one")
+// document.getElementsByClassName("fruit")
+// document.getElementsByTagName("li")
+// // document.getElementsByName("nm")
+
+// let headingOne = document.querySelector('h1')
+// console.log(headingOne.className)
+// headingOne.classList.add("five")
+// headingOne.classList.add("six")
+// headingOne.classList.toggle('six')
+// headingOne.classList.toggle('six')
+
+// let aa = headingOne.getAttribute('id')
+// console.log(aa)
+// headingOne.setAttribute('id','ten')
+// headingOne.setAttribute('data-cy','twelve')
+// headingOne.removeAttribute("id")
---
+++ DOM22/script.js
@@ -0,0 +1,34 @@
+
+let buttonA = document.querySelector('button')
+console.log(buttonA)
+
+let headOne = document.querySelector('h1')
+console.log(headOne)
+
+// buttonA.addEventListener('click',function(){
+//     headOne.style.color = "red"
+// })
+
+// buttonA.addEventListener('dblclick',function(){
+//     headOne.style.color = "red"
+// })
+
+// buttonA.addEventListener('mouseover',function(){
+//     headOne.style.color = "red"
+// })
+
+// buttonA.addEventListener('mouseout',function(){
+//     headOne.style.color = "green"
+// })
+
+// buttonA.addEventListener('click',function(){
+//     window.location.reload();
+
+// })
+
+// Oops
+
+
+
+
+
