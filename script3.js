b1701d8615a9f8edd5d27e216aa313c79174dcc2
unknown <Minskoleopc>
2023-09-14 07:39:37 -0700

    added

---
+++ script3.js
@@ -0,0 +1,54 @@
+// let amol = {
+//     firstName:"amol",
+//     lastName:"rao",
+//     age:28,
+//     skills:["sql","powerBI"]
+// }
+
+// let chinmay = {
+//     firstName:"chinmay",
+//     lastName:"deshpande",
+//     age:34,
+//     skills:["python","c++"]
+// }
+// // 100 object ---- 500 lines of code
+// class Person {
+//     firstName ;
+//     lastName 
+//     age;
+//     skills;
+
+// }
+
+// let amol1 = new Person()
+// let chinmay1 = new Person()
+// console.log(amol1)
+// amol1.firstName = "amol"
+// amol1.lastName= "rao"
+// amol1.skills = ["python","javascript"]
+// amol1.age = 77
+// console.log(amol1)
+// console.log(chinmay1)
+
+
+// program 2
+
+class PersonB {
+    constructor(fn,ln ,age ,skills){
+        this.firstName = fn 
+        this.lastName = ln 
+        this.age = age 
+        this.skills = skills
+    }
+
+}
+let amol3 = new PersonB("amol3", "rao3", 45 , ["html","css"])
+let amol4 = new PersonB("amol4", "rao4", 40 , ["html","css"])
+console.log(amol3)
+console.log(amol4)
+
+
+
+
+
+
